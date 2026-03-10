07d6c5007eba472b852a5191036ac719aaa26b3e
Sophia Blair <sophb.ccjt@proton.me>
2026-03-10 04:20:34 -0300

    Update progress.js

--- progress.js
+++ progress.js
@@ -1,10 +1,14 @@
 function progress(percentage, {
     scale = 10,
     template = '[{bar}] {percent}%',
-    roundingFunc = Math.floor
+    roundingFunc = Math.floor,
+    fullchar = '█',
+    emptychar = '⠀'
 } = {}) {
     scale = roundingFunc(scale);
     percentage = Math.max(0, Math.min(1, percentage));
-    const bar = "█".repeat(roundingFunc(percentage * scale)).padEnd(scale, "⠀");
+    const bar = fullchar
+        .repeat(roundingFunc(percentage * scale))
+        .padEnd(scale, emptychar);
     return template.replaceAll('{bar}', bar).replaceAll('{percent}', roundingFunc(percentage * 100));
 }
