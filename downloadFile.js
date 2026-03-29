d86321f86faf9f42543faef3c38f400578835a68
Sophia Blair <sophb.ccjt@proton.me>
2026-03-28 22:06:51 -0300

    Add function to download file with content

---
+++ downloadFile.js
@@ -0,0 +1,14 @@
+function downloadFileWithContent(filename = `Untitled file [${Date.now()}]`, content = '') {
+    const blob = new Blob([content], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
+    return content;
+}
